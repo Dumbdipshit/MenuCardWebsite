036afa51751b1b5695290bea67e1946bb8dce576
Dumbdipshit <1211675@student.roc-nijmegen.nl>
2025-04-08 13:59:39 +0200

    initial commit
    Added the header not finished

---
+++ Website/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>header</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <header>
+        <div class="header-bar-container">
+            <div class="header-menu-button-container">
+                    <div class="header-menu-button-bar"></div>
+                    <div class="header-menu-button-bar"></div>
+                    <div class="header-menu-button-bar"></div>
+            </div>
+            <div class="logo-and-name-container">
+                    <h1 class="logo-font">Food</h1>
+            </div>
+            <div class="extra-header-button-container">
+                <div class="extra-header-button"></div>
+                <div class="extra-header-button"></div>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
