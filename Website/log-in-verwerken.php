033a6f0d1b9fe6f489e3d88673df4e6f14c3bb72
Dumbdipshit <1211675@student.roc-nijmegen.nl>
2025-04-10 09:55:29 +0200

    update
    log in works but imma try to make it safe to use

--- Website/log-in-verwerken.php
+++ Website/log-in-verwerken.php
@@ -2,15 +2,16 @@
     session_start();
 
     include ("connect.php");
-
     $db = $conn->prepare("
         SELECT * FROM user
+        WHERE username='". $_POST["username"]. "' AND password='". $_POST["password"]."';
     ");
 
     $db->execute();
     $result = $db->fetchAll();
 
     if($result[0]['username'] == $_POST["username"] && $result[0]['password'] == $_POST["password"]){
+        $_SESSION["user"] = $_POST["username"];
         $_SESSION["logInError"] = "false";
         header("Location: index.php");
     }else{
--- Website/log-in.php
+++ Website/log-in.php
@@ -15,7 +15,7 @@
         <div class="header-container-row">
             <h1 class="logo">food</h1>
             <a href="index.php" class="page">menu</a>
-            <a href="log-in.php" class="page">log in</a>
+            <a href="log-in.php" class="page">log in</a><!-- remove if user is logged in-->
             <a href="admin.php" class="page">admin</a> <!-- remove if not logged in as admin -->
         </div>
     </header>
